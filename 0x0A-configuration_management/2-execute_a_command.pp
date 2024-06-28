d2da4ba85a229c3201e7dceb1fdad3d3c8e962bd
Ankubito <prudent.nkubito@gmail.com>
2024-06-28 12:52:41 -0700

    Configguration management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 #Create a manifest that kills a process named killmenow
 
-exec { 'pkill':
-  command => 'pkill -f killmenow',
-  provider => 'shell',
+exec { 'pkill_killmenow':
+  command   => 'pkill -f killmenow',
+  provider  => 'shell',
+  logoutput => true,
 }
