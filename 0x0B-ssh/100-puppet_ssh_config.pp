4c80042d5629ea816601afdfe856c482244abae2
mumugisha <prudent.nkubito@gmail.com>
2024-07-23 22:55:51 +0300

    Firewall

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,25 +1,22 @@
-# mymodule/manifests/init.pp
-
-# Class to manage SSH configuration
-class mymodule::ssh_config {
-  file { '/etc/ssh/ssh_config':
-    ensure => 'present',
-  }
-
-  # Ensure 'passwordAuthentication no' is present in /etc/ssh/ssh_config
-  file_line { 'ensure_password_authentication_no':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => 'passwordAuthentication no',
-    match  => '^passwordAuthentication',
-  }
-
-  # Ensure 'IdentityFile ~/.ssh/school' is present in /etc/ssh/ssh_config
-  file_line { 'ensure_identity_file_school':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => 'IdentityFile ~/.ssh/school',
-    match  => '^IdentityFile',
-  }
+#!/usr/bin/env bash
+##make changes to your config files
+file { '/etc/ssh/ssh_config':
+  ensure => 'present',
 }
 
+# Ensure 'passwordAuthentication no' is present in /etc/ssh/ssh_config
+file_line { 'ensure_password_authentication_no':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'passwordAuthentication no',
+  match   => '^passwordAuthentication yes',
+  replace => 'true',
+}
+
+# Ensure 'IdentityFile ~/.ssh/school' is present in /etc/ssh/ssh_config
+file_line { 'ensure_identity_file_school':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/config',
+  match  => '^IdentityFile',
+}
