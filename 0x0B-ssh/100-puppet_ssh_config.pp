88137f746dc0bffb3f4de55e9089fd2cc5fcd643
mumugisha <prudent.nkubito@gmail.com>
2024-07-23 21:52:12 +0300

    Firewall

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+#make changes to your config files
+
+file {'etc/ssh/ssh_config':
+}
+
 # Ensure 'passwordAuthentication no' is present in etc/ssh/ssh-config
 file_line { 'ensure_password_authentication_no':
   path   => '/etc/ssh/ssh-config',
@@ -9,7 +15,7 @@ file_line { 'ensure_password_authentication_no':
 # Ensure 'IdentityFile ~/.ssh/school' is present in etc/ssh/ssh-config
 file_line { 'ensure_identity_file_school':
   path   => '/etc/ssh/ssh-config',
-  line   => 'IdentityFile ~/.ssh/id_rsa',
+  line   => 'IdentityFile ~/.ssh/config',
   match  => '^IdentityFile',
   ensure => present,
 }
