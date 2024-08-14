7e4f954eb67389c760388c7a1772819ac733e6d4
mumugisha <prudent.nkubito@gmail.com>
2024-08-14 12:07:55 -0700

    Web Stack Debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# find out why Apache is returning a 500 error.
+# Once you find the issue, fix it and then automate it using Puppet
+
+exec { replace_phpp_with_php:
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+}
