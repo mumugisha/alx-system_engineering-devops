703e49aab07d87963368605469fbac7fe47fbffb
mumugisha <prudent.nkubito@gmail.com>
2024-08-18 10:53:22 -0700

    Postmortem

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,12 @@
 """
 Function that queries the Reddit API and returns the number of subscribers.
 """
-
-
 import requests
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'custom user agent'}
+    headers = {'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
