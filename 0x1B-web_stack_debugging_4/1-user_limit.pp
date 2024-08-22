7281260b9a7ff5c487c907e89338ea5edf79a999
mumugisha <prudent.nkubito@gmail.com>
2024-08-22 23:49:06 +0300

    web stack debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,13 @@
-# Increase the ULIMIT for Holberton user
+# This manifest increases the soft and hard file descriptor limits for the holberton user.
+
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf",
-  path    => '/usr/local/bin:/bin/'
+ command => 'sed -i \'/^holberton.*hard.*nofile/s/[0-9]\\+/50000/\' /etc/security/limits.conf',
+ path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+ onlyif  => 'grep -q "^holberton.*hard.*nofile" /etc/security/limits.conf',
 }
 
-# Increase the ULIMIT for Holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => " sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin:/bin/'
+ command => 'sed -i \'/^holberton.*soft.*nofile/s/[0-9]\\+/50000/\' /etc/security/limits.conf',
+ path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+ onlyif  => 'grep -q "^holberton.*soft.*nofile" /etc/security/limits.conf',
 }
-
