03db7a9823a9c3dd87880ca1526ea3af1ef0a436
mumugisha <prudent.nkubito@gmail.com>
2024-08-22 14:55:59 +0300

    Web stack debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # Increase the ULIMIT for Holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin:/bin/',
+  command => "sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin/'
 }
 
 # Increase the ULIMIT for Holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => " sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin:/bin/',
+  path    => '/usr/local/bin:/bin/'
 }
 
