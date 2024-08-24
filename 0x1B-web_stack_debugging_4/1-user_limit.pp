dea1f8f3de47aca2199085ca5c4e798f1c64d64c
Alphonse Mugisha <79317323+mumugisha@users.noreply.github.com>
2024-08-24 10:09:01 +0200

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # This manifest adjusts the OS configuration to allow the holberton user to log in and open files without issues
 
-file_line { 'set_holberton_hard_nofile':
-  path  => '/etc/security/limits.conf',
-  line  => 'holberton hard nofile 50000',
-  match => '^holberton\s+hard\s+nofile',
+exec { 'set_holberton_hard_nofile':
+  command => 'echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  unless  => 'grep -q "^holberton hard nofile" /etc/security/limits.conf',
 }
 
-file_line { 'set_holberton_soft_nofile':
-  path  => '/etc/security/limits.conf',
-  line  => 'holberton soft nofile 50000',
-  match => '^holberton\s+soft\s+nofile',
+exec { 'set_holberton_soft_nofile':
+  command => 'echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  unless  => 'grep -q "^holberton soft nofile" /etc/security/limits.conf',
 }
