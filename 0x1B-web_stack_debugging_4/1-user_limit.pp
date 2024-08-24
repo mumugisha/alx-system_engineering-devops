0b3cbb89592b85e32687eb18d125415dac4be183
mumugisha <prudent.nkubito@gmail.com>
2024-08-24 10:55:06 +0300

    Web stack debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # This manifest adjusts the OS configuration to allow the holberton user to log in and open files without issues
 
-exec { 'increase-file-limits-for-holberton-user':
+exec {'increase-file-limits-for-holberton-user':
   command => 'sed -i -e \'/^holberton.*hard.*nofile/s/[0-9]\\+/50000/\' -e \'/^holberton.*soft.*nofile/s/[0-9]\\+/50000/\' /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/usr/bin', '/bin'],
   onlyif  => 'grep -q "^holberton.*hard.*nofile" /etc/security/limits.conf && grep -q "^holberton.*soft.*nofile" /etc/security/limits.conf',
