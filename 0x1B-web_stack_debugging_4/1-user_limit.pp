1d71e8b13bb0c55655726e82731ecb23498725d9
Alphonse Mugisha <79317323+mumugisha@users.noreply.github.com>
2024-08-24 09:48:57 +0200

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,7 @@
-# This manifest increases the soft and hard file descriptor limits for the holberton user
+# This manifest adjusts the OS configuration to allow the holberton user to log in and open files without issues
 
-exec { 'increase-hard-file-limit-for-holberton-user':
- command => 'sed -i \'/^holberton.*hard.*nofile/s/[0-9]\\+/50000/\' /etc/security/limits.conf',
- path    => ['/usr/local/bin', '/usr/bin', '/bin'],
- onlyif  => 'grep -q "^holberton.*hard.*nofile" /etc/security/limits.conf',
-}
-
- exec { 'increase-soft-file-limit-for-holberton-user':
- command => 'sed -i \'/^holberton.*soft.*nofile/s/[0-9]\\+/50000/\' /etc/security/limits.conf',
- path    => ['/usr/local/bin', '/usr/bin', '/bin'],
- onlyif  => 'grep -q "^holberton.*soft.*nofile" /etc/security/limits.conf',
+exec { 'increase-file-limits-for-holberton-user':
+  command => 'sed -i -e \'/^holberton.*hard.*nofile/s/[0-9]\\+/50000/\' -e \'/^holberton.*soft.*nofile/s/[0-9]\\+/50000/\' /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  onlyif  => 'grep -q "^holberton.*hard.*nofile" /etc/security/limits.conf && grep -q "^holberton.*soft.*nofile" /etc/security/limits.conf',
 }
