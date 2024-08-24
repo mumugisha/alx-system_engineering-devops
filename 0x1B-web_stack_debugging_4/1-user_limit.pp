ec913054d5d55a7f3aeb363a728bd89f59c10ae2
Alphonse Mugisha <79317323+mumugisha@users.noreply.github.com>
2024-08-24 12:29:03 +0200

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,13 @@
-# This manifest adjusts the OS configuration to allow the holberton user to log in and open files without issues
+# Your Puppet manifest adjusts the OS configuration so that it is possible to log in with the holberton user and open a file without issue
 
-exec { 'set_holberton_nofile_limits':
-  command => 'echo -e "holberton hard nofile 50000\nholberton soft nofile 50000" >> /etc/security/limits.conf',
+exec { 'set_holberton_hard_nofile':
+  command => 'echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/usr/bin', '/bin'],
-  unless  => 'grep -q "^holberton hard nofile" /etc/security/limits.conf && grep -q "^holberton soft nofile" /etc/security/limits.conf',
+  unless  => 'grep -q "^holberton hard nofile" /etc/security/limits.conf',
+}
+
+exec { 'set_holberton_soft_nofile':
+  command => 'echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  unless  => 'grep -q "^holberton soft nofile" /etc/security/limits.conf',
 }
