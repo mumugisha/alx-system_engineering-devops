64ad5fd3b4e7fc87fdd73c61d69863dac6869694
mumugisha <prudent.nkubito@gmail.com>
2024-12-15 22:22:33 +0200

    API Advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Top ten number of subscribers
+"""
+
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+    user_agent = {'User-Agent': 'lizzie'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    try:
+        response = requests.get(url, headers=user_agent, timeout=10)
+        if response.status_code != 200:
+            print(None)
+            return
+
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post.get('data', {}).get('title'))
+    except Exception as e:
+        print(None)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        top_ten(sys.argv[1])
+    else:
+        print(None)
